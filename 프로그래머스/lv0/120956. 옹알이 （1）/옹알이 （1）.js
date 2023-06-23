9e54108f67f6027dcc14191a09a6b6d792de54ce
kuetiipp <130050839+kuetiipp@users.noreply.github.com>
2023-06-23 10:38:58 +0900

    [level 0] Title: 옹알이 (1), Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,52 +1,23 @@
 function solution(babbling) {
     const baby = ["aya", "ye", "woo", "ma"];
-    let newCase = ["aya", "ye", "woo", "ma"];
-    let caseStr = "";
+    let newCase = new Map();
     let num = 0;
-    //2
-    for(let i = 0; i < baby.length; i++){
-        for(let k = 0; k < baby.length; k++){
-            if(i !== k){
-                newCase.push(baby[i].concat(baby[k]));
-            }
-        }
-    }
-    //3
-    for(let i = 0; i < baby.length; i++){
-        for(let k = 0; k < baby.length; k++){
-            if(i !== k){
-                for(let j = 0; j < baby.length; j++){
-                    if(k !== j && i !== j){
-                        newCase.push(baby[i]+baby[k]+baby[j]);
-                    }
-                }
-            }
-        }
-    }
-    //4
-    for(let i = 0; i < baby.length; i++){
-        for(let k = 0; k < baby.length; k++){
-            if(i !== k){
-                for(let j = 0; j < baby.length; j++){
-                    if(k !== j && i !== j){
-                       for(let l = 0; l < baby.length; l++){
-                           if(i !== l && k !== l && j !==l){
-                           newCase.push(baby[i]+baby[k]+baby[j]+baby[l]);
-                           }
-                       }
-                    }
-                }
-            }
-        }
-    }
-    for(let i = 0; i < newCase.length; i ++){
-        for(let k = 0; k < babbling.length; k++){
-            if(newCase[i] === babbling[k]){
-                num++;
-            }
-        }
-    }
     
-    console.log(newCase);
+    function duple(arr){
+        newCase.set(arr.join(''), true);
+        for(let i = 0; i < baby.length; i++){
+            if(arr.includes(baby[i]) === false){
+                let newArr = [...arr];
+                newArr.push(baby[i]);
+                duple(newArr);
+            }
+        }
+    }
+    duple([]);
+    for(let i = 0; i < babbling.length; i++){
+        if(newCase.has(babbling[i])){
+            num++;
+        }
+    }
     return num;
 }
